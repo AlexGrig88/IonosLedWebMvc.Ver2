588091b5f2f103210573d609b3cda46dae81fa69
Aleksandr Grigorenko <grigxia86@gmail.com>
2025-05-02 11:18:29 +0300

    created sidebar with menu

--- IonosLedWebMvc.Ver2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace IonosLedWebMvc.Ver2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
